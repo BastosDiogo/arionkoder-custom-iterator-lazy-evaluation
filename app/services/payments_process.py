455227c2f9054dfa5754ee8feb17a02e36c1c37a
bmnetto.diogo@gmail.com <bmnetto.diogo@gmail.com>
2025-06-25 15:25:43 -0300

    Add endpoints.

--- app/services/payments_process.py
+++ app/services/payments_process.py
@@ -1,8 +1,5 @@
-import logging
 from services.mock_payments import Mock
 from services.lazy_collection import LazyCollection
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Payments:
     """Class to process and manage payments."""
@@ -20,5 +17,24 @@ class Payments:
 
     def created_before_month(self, month: int, page: int, page_size: int):
         """Filter payments created before or in a given month."""
-        payment = self._data.filter(lambda p: int(p['created_at'][5:7]) <= month)
+        payment = self._data.filter(lambda payment: int(payment['created_at'][5:7]) <= month)
         return self.paginate(payment, page, page_size)
+
+    def payment_groups(self, group_size:int, page:int, page_size:int):
+        """Divide return payments into groups."""
+        payments = self._data.chunk(group_size)
+        return self.paginate(payments, page, page_size)
+
+    def trimestal_payments(self):
+        """Return payments per trimestre."""
+        trimestral = {
+            'jan_to_mar': {'payments': []},
+            'abr_to_jun': {'payments': []}
+        }
+
+        payments_jan_to_mar = self._data.filter(lambda payment: int(payment['created_at'][5:7]) <= 3).to_list()
+        payments_abr_to_jun = self._data.filter(lambda payment: 4 <= int(payment['created_at'][5:7]) <= 6).to_list()
+        trimestral['jan_to_mar']['payments'] = payments_jan_to_mar
+        trimestral['abr_to_jun']['payments'] = payments_abr_to_jun
+
+        return trimestral
